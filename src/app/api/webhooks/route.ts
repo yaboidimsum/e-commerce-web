7d03f8ff923bb5a95a75f081595e74e661817cc1
Dimas Prihady Setyawan <101172637+yaboidimsum@users.noreply.github.com>
2024-06-12 19:30:27 +0700

    fix: resend

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,9 +72,9 @@ export async function POST(req: Request) {
       });
 
       await resend.emails.send({
-        from: "5025211184@mhs.its.ac.id",
-        to: "dprihadisetiawan@gmail.com",
-        // to: [event.data.object.customer_details.email],
+        from: "dprihadisetiawan@gmail.com",
+        //to: "dprihadisetiawan@gmail.com",
+        to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
           orderId,
